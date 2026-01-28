5c4c6a1e8a2e87185893c8278850aae72eeba452
Agus Indra <agusindra020405@gmail.com>
2026-01-28 20:13:49 +0800

    menambahkan komentar pada bagian views update

--- Views/product/update.php
+++ Views/product/update.php
@@ -35,7 +35,7 @@ $conn == null;
     <title>Create Product</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-
+<!-- bagian body -->
 <body>
     <div class="container">
         <h2>Update Product</h2>
